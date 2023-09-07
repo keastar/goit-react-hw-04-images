fe6105cd19bcf41ab7c2a235916b5411d052b4bb
keastar <keastar1986@gmail.com>
2023-09-07 16:30:01 +0300

    fixing SearchBar
    fixing SearchBar/ Done 04 task from 4 HW!

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,58 +1,54 @@
 import PropTypes from 'prop-types';
 import { ImSearch } from 'react-icons/im';
 import css from './Searchbar.module.css';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export default class Searchbar extends Component {
-  state = {
-    searchImgName: '',
+export default function Searchbar({ onSubmit }) {
+  const [searchImgName, setSearchImgName] = useState('');
+
+  const handleNameChange = event => {
+    setSearchImgName(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = event => {
-    this.setState({ searchImgName: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.searchImgName.trim() === '') {
+    if (searchImgName.trim() === '') {
       return alert('Введите, что хотите найти из изображений!');
     }
-    //2.И передаю в метод значение из этого файла state=searchName -> прокидываем пропс с App 'propName' и передаем ему значение с Searchbar -> searchImgName
-    this.props.onSubmit(this.state.searchImgName);
+    //2.И передаю в метод значение из этого файла state=searchImgName -> прокидываем пропс с App 'propName' и передаем ему значение с Searchbar -> searchImgName
+    onSubmit(searchImgName);
     //5.Идет очистка формы поля input после ее сабмита
-    this.setState({ searchImgName: '' });
+    setSearchImgName('');
   };
 
-  render() {
-    return (
-      <>
-        <header className={css.searchbar}>
-          {/* // 1.при событии Submit формы -> вызываю метод из App -> handleSubmit -> this.props.propName */}
-          <form className={css.form} onSubmit={this.handleSubmit}>
-            <button type="submit" className={css.button}>
-              <ImSearch
-                style={{
-                  height: '20',
-                  width: '20',
-                }}
-              />
-            </button>
-            <input
-              className={css.input}
-              type="text"
-              name="searchImgName"
-              value={this.state.searchImgName}
-              onChange={this.handleNameChange}
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
+  return (
+    <>
+      <header className={css.searchbar}>
+        {/* // 1.при событии Submit формы -> вызываю из App -> handleSubmit -> propName */}
+        <form className={css.form} onSubmit={handleSubmit}>
+          <button type="submit" className={css.button}>
+            <ImSearch
+              style={{
+                height: '20',
+                width: '20',
+              }}
             />
-          </form>
-        </header>
-      </>
-    );
-  }
+          </button>
+          <input
+            className={css.input}
+            type="text"
+            name="searchImgName"
+            value={searchImgName}
+            onChange={handleNameChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    </>
+  );
 }
 
 Searchbar.propTypes = {
